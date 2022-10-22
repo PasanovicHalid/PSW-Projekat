2fdbcea5dcca8231447e1a5a42aafd67273f22b3
unknown <tatjana.gemovic@icodefactory.com>
2022-10-22 19:36:05 +0200

    Change database

---
+++ PSW-projekat/hospital-be-main/src/HospitalLibrary/Migrations/20221022173452_addFeedbacks.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HospitalLibrary.Migrations
+{
+    public partial class addFeedbacks : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Feedbacks",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Feedbacks");
+        }
+    }
+}
