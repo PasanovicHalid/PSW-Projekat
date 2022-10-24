209befbfd216af55a48ee9f2681b0e43074939e4
Radiša Stojkić <stojkic.ra84.2019@uns.ac.rs>
2022-10-25 01:06:44 +0200

    Implemented email sending

--- PSW-projekat/hospital-be-main/src/HospitalLibrary/Core/Service/AppointmentService.cs
+++ PSW-projekat/hospital-be-main/src/HospitalLibrary/Core/Service/AppointmentService.cs
@@ -3,6 +3,8 @@ using HospitalLibrary.Core.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -24,6 +26,25 @@ namespace HospitalLibrary.Core.Service
 
         public void Delete(Appointment entity)
         {
+            string fromMail = "radisa.stojkic@gmail.com";
+            string fromPassword = "pblclisovitnnlfz";
+
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress(fromMail);
+            message.Subject = "Test Subject";
+            message.To.Add(new MailAddress("radisa.stojkic@gmail.com"));
+            message.Body = "<html><body> Termin za pregled je odlozen. </body></html>";
+            message.IsBodyHtml = true;
+
+            var smtpClient = new SmtpClient("smtp.gmail.com")
+            {
+                Port = 587,
+                Credentials = new NetworkCredential(fromMail, fromPassword),
+                EnableSsl = true,
+            };
+
+            smtpClient.Send(message);
+
             _appointmentRepository.Delete(entity);
         }
 
