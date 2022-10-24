5c9598be28ede20573858ded91980419dad79d87
Radiša Stojkić <stojkic.ra84.2019@uns.ac.rs>
2022-10-24 19:27:22 +0200

    basic backend logic for scheduling appointments

---
+++ PSW-projekat/hospital-be-main/src/HospitalLibrary/Core/Service/AppointmentService.cs
@@ -0,0 +1,45 @@
+﻿using HospitalLibrary.Core.Model;
+using HospitalLibrary.Core.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalLibrary.Core.Service
+{
+    public class AppointmentService : IService<Appointment>
+    {
+        private readonly IRepository<Appointment> _appointmentRepository;
+
+        public AppointmentService(IRepository<Appointment> appointmentRepository)
+        {
+            _appointmentRepository = appointmentRepository;
+        }
+
+        public void Create(Appointment entity)
+        {
+            _appointmentRepository.Create(entity);
+        }
+
+        public void Delete(Appointment entity)
+        {
+            _appointmentRepository.Delete(entity);
+        }
+
+        public IEnumerable<Appointment> GetAll()
+        {
+            return _appointmentRepository.GetAll();
+        }
+
+        public Appointment GetById(int id)
+        {
+            return _appointmentRepository.GetById(id);
+        }
+
+        public void Update(Appointment entity)
+        {
+            _appointmentRepository.Update(entity);
+        }
+    }
+}
