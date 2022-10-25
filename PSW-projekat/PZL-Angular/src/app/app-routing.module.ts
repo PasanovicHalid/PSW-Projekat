43303100cc5fd4a8f3814a496aa4cc6d9d640624
mihajlo-ra92 <maksimovic.ra92.2019@uns.ac.rs>
2022-10-25 18:34:35 +0200

    Create blood bank module, import mat-table

--- PSW-projekat/PZL-Angular/src/app/app-routing.module.ts
+++ PSW-projekat/PZL-Angular/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BloodBanksComponent } from './modules/blood-bank/blood-banks/blood-banks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'blood-banks', component: BloodBanksComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
