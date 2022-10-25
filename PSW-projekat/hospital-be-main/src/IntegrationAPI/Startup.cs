5b00e8d07d512b8ab368881592a63fed14fa1d79
Halid Pasanovic <halidpasanovic1000@gmail.com>
2022-10-25 15:31:51 +0200

    Addition of initial migration

--- PSW-projekat/hospital-be-main/src/IntegrationAPI/Startup.cs
+++ PSW-projekat/hospital-be-main/src/IntegrationAPI/Startup.cs
@@ -45,7 +45,6 @@ namespace IntegrationAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IntegrationDbContext integrationDbContext)
         {
-            integrationDbContext.Database.EnsureCreated();
             app.UseHttpsRedirection();
 
             if (env.IsDevelopment())
