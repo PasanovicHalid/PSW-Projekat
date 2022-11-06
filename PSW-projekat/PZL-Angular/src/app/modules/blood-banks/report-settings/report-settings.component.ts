8b1a07484b1a36ed97d9ffff767c1d2247aaea3d
Halid Pasanovic <halidpasanovic1000@gmail.com>
2022-11-06 18:35:00 +0100

    Creation of module for blood banks

---
+++ PSW-projekat/PZL-Angular/src/app/modules/blood-banks/report-settings/report-settings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-report-settings',
+  templateUrl: './report-settings.component.html',
+  styleUrls: ['./report-settings.component.css']
+})
+export class ReportSettingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
