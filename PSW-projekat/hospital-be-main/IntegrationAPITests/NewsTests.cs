37e4d8567e7e0bda7b1e5b60940f1e566690e561
mihajlo-ra92 <maksimovic.ra92.2019@uns.ac.rs>
2022-11-09 17:24:46 +0100

    Finish first unit test for news

--- PSW-projekat/hospital-be-main/IntegrationAPITests/NewsTests.cs
+++ PSW-projekat/hospital-be-main/IntegrationAPITests/NewsTests.cs
@@ -1,4 +1,7 @@
-﻿using Moq;
+﻿using IntegrationLibrary.Core.Model;
+using IntegrationLibrary.Core.Repository.Newses;
+using IntegrationLibrary.Core.Service.Newses;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,9 +14,25 @@ namespace IntegrationAPITests
     public class NewsTests
     {
         [Fact]
-        public void GetNotFinished()
+        public void Test1()
         {
-            var stubRepo = new Mock<>
+            var stubRepo = new Mock<INewsRepository>();
+            var newses = new List<News>();
+            News n1 = new News()
+            {
+                Id = 1,
+                Name = "Blood donation 1",
+                DateTime = new DateTime(2022, 01, 01, 9, 15, 0),
+                BloodBankId = 1,
+            };
+            newses.Add(n1);
+            stubRepo.Setup(m => m.GetAll()).Returns(newses);
+            stubRepo.Setup(m => m.GetById(1)).Returns(n1);
+
+            NewsService newsService = new NewsService(stubRepo.Object);
+
+            News n = newsService.GetById(1);
+            Assert.NotNull(n);
         }
     }
 }
