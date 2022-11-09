3160563e240e88676f21385e4218dbbfcb534e06
Radiša Stojkić <stojkic.ra84.2019@uns.ac.rs>
2022-11-09 19:17:50 +0100

    Created integration test to create blood request

---
+++ PSW-projekat/hospital-be-main/src/IntegrationAPI/Adapters/BloodRequestAdapter.cs
@@ -0,0 +1,28 @@
+﻿using IntegrationAPI.DTO;
+using IntegrationLibrary.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationAPI.Adapters
+{
+    public class BloodRequestAdapter
+    {
+        public static BloodRequest FromDTO(BloodRequestDTO entity)
+        {
+            return new BloodRequest()
+            {
+              
+            };
+        }
+
+        public static BloodRequestDTO ToDTO(BloodRequest entity)
+        {
+            return new BloodRequestDTO()
+            {
+              
+            };
+        }
+    }
+}
---
+++ PSW-projekat/hospital-be-main/src/IntegrationAPI/Controllers/BloodRequestController.cs
@@ -0,0 +1,33 @@
+﻿using IntegrationAPI.Adapters;
+using IntegrationAPI.DTO;
+using IntegrationLibrary.Core.Model;
+using IntegrationLibrary.Core.Service.BloodRequests;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationAPI.Controllers
+{
+    [EnableCors]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BloodRequestController : ControllerBase
+    {
+
+
+        private IBloodRequestService _bloodRequestService;
+
+        public BloodRequestController(IBloodRequestService bloodRequestService)
+        {
+            this._bloodRequestService = bloodRequestService;
+        }
+
+        public OkObjectResult Create(BloodRequestDTO testCase)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
