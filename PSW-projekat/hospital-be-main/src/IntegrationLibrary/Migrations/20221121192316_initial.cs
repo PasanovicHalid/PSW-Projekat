e22031807f55f86f6a8a6d3ca533f5f2d2000faf
Halid Pasanovic <halidpasanovic1000@gmail.com>
2022-11-21 20:23:31 +0100

    Adding migration

---
+++ PSW-projekat/hospital-be-main/src/IntegrationLibrary/Migrations/20221121192316_initial.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IntegrationLibrary.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BloodBanks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ServerAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ApiKey = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordResetKey = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AccountStatus = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BloodBanks", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BloodRequests",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RequiredForDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BloodQuantity = table.Column<int>(type: "int", nullable: false),
+                    Reason = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DoctorId = table.Column<int>(type: "int", nullable: false),
+                    RequestState = table.Column<int>(type: "int", nullable: false),
+                    BloodType = table.Column<int>(type: "int", nullable: false),
+                    BloodBankId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BloodRequests", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Newses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    _name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    _dateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    _bloodBankId = table.Column<int>(type: "int", nullable: false),
+                    _status = table.Column<int>(type: "int", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    DateTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BloodBankId = table.Column<int>(type: "int", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Newses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ReportSettings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StartDeliveryDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DeliveryYears = table.Column<int>(type: "int", nullable: false),
+                    DeliveryMonths = table.Column<int>(type: "int", nullable: false),
+                    DeliveryDays = table.Column<int>(type: "int", nullable: false),
+                    CalculationYears = table.Column<int>(type: "int", nullable: false),
+                    CalculationMonths = table.Column<int>(type: "int", nullable: false),
+                    CalculationDays = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ReportSettings", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "BloodBanks",
+                columns: new[] { "Id", "AccountStatus", "ApiKey", "Email", "Name", "Password", "PasswordResetKey", "ServerAddress" },
+                values: new object[] { 1, 1, "sadfasdads", "asdasd@gmail.com", "asdsadsda", "asdsadsdadas", null, "https://www.messenger.com/t/100001603572170" });
+
+            migrationBuilder.InsertData(
+                table: "ReportSettings",
+                columns: new[] { "Id", "CalculationDays", "CalculationMonths", "CalculationYears", "DeliveryDays", "DeliveryMonths", "DeliveryYears", "StartDeliveryDate" },
+                values: new object[] { 1, 0, 1, 0, 0, 1, 0, new DateTime(2022, 11, 21, 0, 0, 0, 0, DateTimeKind.Local) });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BloodBanks");
+
+            migrationBuilder.DropTable(
+                name: "BloodRequests");
+
+            migrationBuilder.DropTable(
+                name: "Newses");
+
+            migrationBuilder.DropTable(
+                name: "ReportSettings");
+        }
+    }
+}
