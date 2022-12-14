b34360a4359347d8956bc7efce1c6e9e5f18b039
Radiša Stojkić <stojkic.ra84.2019@uns.ac.rs>
2022-12-14 09:17:40 +0100

    Supplemented test for consilium

--- PSW-projekat/hospital-be-main/HospitalTests/Integration/CouncilOfDoctorsTests.cs
+++ PSW-projekat/hospital-be-main/HospitalTests/Integration/CouncilOfDoctorsTests.cs
@@ -3,6 +3,7 @@ using HospitalAPI.Controllers.PrivateApp;
 using HospitalAPI.DTO;
 using HospitalLibrary.Core.DTOs;
 using HospitalLibrary.Core.Model;
+using HospitalLibrary.Core.Model.Enums;
 using HospitalLibrary.Core.Service.CouncilOfDoctors;
 using HospitalTests.Setup;
 using Microsoft.AspNetCore.Mvc;
@@ -35,15 +36,27 @@ namespace HospitalTests.Integration
         {
             using var scope = Factory.Services.CreateScope();
             var controller = SetupSettingsController(scope);
-
+            List<Specialization> specialization = new List<Specialization>();
+            specialization.Add(Specialization.general);
+            specialization.Add(Specialization.neurologist);
+            ICollection<DoctorDto> doctors = new List<DoctorDto>();
 
             DoctorCouncilDTO testCase = new DoctorCouncilDTO()
             {
+                 Id= 0,
+                DoctorId= 1,
+                Topic= "Tema",
+                Doctors = doctors,
+                Specializations=specialization,
+                Start=new DateTime(2022,12,12,7,42,0),
+                End= new DateTime(2022, 12, 15, 7, 42, 0),
+                Duration= 0
             };
 
             var result = ((OkObjectResult)controller.Create(testCase))?.Value as DoctorCouncilDTO;
             Assert.NotNull(result);
             result.ShouldBe(testCase);
+            Assert.Equal(result.DoctorId, testCase.DoctorId);
         }
 
         [Fact]
